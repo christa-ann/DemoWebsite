52a6d124c507ee7664736a537144dd3c289ae056
christa-ann <88717435+christa-ann@users.noreply.github.com>
2021-08-11 19:53:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 	<title>Demo Index</title>
 </head>
 <body>
-<h1>Hi All</h1>
+<h1>Welcome All</h1>
+<form>
+	<h4>Login</h4>
+	<input type="text" name="username"  class="form-control">
+	<input type="password" name="password" class="form-control">
+	<button type="submit"  class="form-control"> Login </button>
+</form>
 </body>
 </html>

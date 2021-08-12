9b529b98539a66c686ab06299e3918a82b8e36c0
Christa Philip <70382391+christa-philip@users.noreply.github.com>
2021-08-12 17:53:46 +0530

    Adding bootstrap classess

--- index.php
+++ index.php
@@ -5,14 +5,22 @@
 		<!-- CSS only -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 </head>
-<body>
-<h1>Welcome All</h1>
-<form>
-	<h4>Login</h4>
-	<input type="text" name="username"  class="form-control">
-	<input type="password" name="password" class="form-control">
-	<button type="submit"  class="form-control"> Login </button>
-</form>
+<body >
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-12 col-lg-12 colxlg-12">
+				<h1>Welcome All</h1>
+				<form>
+					<h4>Login</h4>
+					<input type="text" name="username"  class="form-control">
+					<input type="password" name="password" class="form-control">
+					<button type="submit"  class="form-control"> Login </button>
+				</form>
+			</div>
+		</div>
+	</div>
+	     
+
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>

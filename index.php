703c0e6bcbf98ca216282e28f1c9d9a3f99bd888
christa-ann <88717435+christa-ann@users.noreply.github.com>
2021-08-14 17:04:30 +0530

    Design changes
    changes on index page

--- index.php
+++ index.php
@@ -4,19 +4,31 @@
 	<title>Demo Index</title>
 		<!-- CSS only -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	<style type="text/css">
+		input,button{
+			width: 50%; margin-top: 10px;
+		}
+	</style>
 </head>
-<body style="background:black;" >
+<body style="background:#ccc;" >
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-12 col-md-12 col-lg-12 colxlg-12">
+			<div class="col-sm-12 col-md-12 col-lg-12 colxlg-12 text-center">
 				<h1>Welcome All</h1>
+			</div>
+			<div class="col-sm-3 col-md-3 col-lg-3 colxlg-3"></div>
+			<div class="col-sm-6 col-md-6 col-lg-6 colxlg-6 text-center">
 				<form>
 					<h4>Login</h4>
-					<input type="text" name="username"  class="form-control">
+					<div class="form-group">
+						<input type="text" name="username"  class="form-control">
+					</div>
+					
 					<input type="password" name="password" class="form-control">
-					<button type="submit"  class="form-control"> Login </button>
+					<button type="submit"  class="form-control btn btn-success"> Login </button>
 				</form>
 			</div>
+			<div class="col-sm-3 col-md-3 col-lg-3 colxlg-3"></div>
 		</div>
 	</div>
 	     
